4a6000fc2a7e5c144cd122293aaa85706e3d8251
jovialp <jvlthms@gmail.com>
2022-02-08 14:03:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-j128z9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-j128z9)
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './style.css';
+
+export default function App() {
+  const [nameArray, setNameArray] = useState([]);
+  const [name, setName] = useState('');
+
+  const handleChange = (e) => {
+    if (e.target.value) {
+      setName(e.target.value);
+    } else {
+      setName('');
+    }
+  };
+
+  const submitName = () => {
+    if (name) {
+      const newNameArray = [...nameArray];
+      newNameArray.push(name);
+      setNameArray(newNameArray);
+    }
+    setName('');
+  };
+
+  return (
+    <div>
+      <div>
+        <input type="text" value={name} onChange={handleChange} style={{display:"block"}} />
+        <button onClick={submitName} disabled={!name}>
+          Submit
+        </button>
+      </div>
+      <div>
+        {nameArray.map((submitedName, i) => (
+          <div>{submitedName}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
